720d4e696d57d78710c74bfd29055bd6047c5884
erp12 <erp12@hampshire.edu>
2015-12-31 04:02:56 -0500

    Removed FuseTools version of app. Switching completely to Unity.

---
+++ PickItOut/Assets/Scripts/MainMenuBehavior.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuBehavior : MonoBehaviour {
+
+	Canvas mainCanvas;
+
+	Button flipCoinBtn;
+	Button rollDiceBtn;
+	Button customBtn;
+	Button supportBtn;
+
+	public GameObject flipCoinPrefab;
+	public GameObject rollDicePrefab;
+	public GameObject customPrefab;
+	public GameObject supportPrefab;
+
+	// Ad Stuff
+	public GameObject adPanel;
+
+	// Use this for initialization
+	void Awake () {
+		// App Stuff
+		mainCanvas = GameObject.FindGameObjectWithTag ("MainCanvas").GetComponent<Canvas> ();
+		flipCoinBtn = GameObject.FindGameObjectWithTag ("FlipCoinBtn").GetComponent<Button>();
+		rollDiceBtn = GameObject.FindGameObjectWithTag ("RollDiceBtn").GetComponent<Button>();
+		customBtn = GameObject.FindGameObjectWithTag ("CustomBtn").GetComponent<Button>();
+		supportBtn = GameObject.FindGameObjectWithTag ("SupportBtn").GetComponent<Button>();
+		adPanel = GameObject.FindGameObjectWithTag ("AdPopup");
+		adPanel.SetActive (false);
+
+		flipCoinBtn.onClick.AddListener(() => {
+			PlayRandSound();
+			GameObject page = Instantiate(flipCoinPrefab) as GameObject;
+			page.transform.SetParent(mainCanvas.transform, false);
+			page.transform.SetAsLastSibling();
+		});
+		rollDiceBtn.onClick.AddListener(() => {
+			PlayRandSound();
+			GameObject page = Instantiate(rollDicePrefab) as GameObject;
+			page.transform.SetParent(mainCanvas.transform, false);
+			page.transform.SetAsLastSibling();
+		});
+		customBtn.onClick.AddListener(() => {
+			PlayRandSound();
+			GameObject page = Instantiate(customPrefab) as GameObject;
+			page.transform.SetParent(mainCanvas.transform, false);
+			page.transform.SetAsLastSibling();
+		});
+		supportBtn.onClick.AddListener(() => {
+			PlayRandSound();
+			//PlayAVideo("vze08d53ef040643c598");
+			adPanel.SetActive(true);
+			adPanel.GetComponent<AdPanelBehavior>().SetAdBtnState();
+		});
+
+		// AdColony Stuff
+		InitializeAdStuff ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayRandSound() {
+		AudioClip sfx = Resources.Load ("Sound/"+Random.Range(1,10)) as AudioClip;
+		AudioSource.PlayClipAtPoint (sfx, Vector3.zero);
+	}
+
+	public void InitializeAdStuff()
+	{
+		// Assign any AdColony Delegates before calling Configure
+		AdColony.OnVideoFinished = this.OnVideoFinished;
+		
+		// If you wish to use a the customID feature, you should call  that now.
+		// Then, configure AdColony:
+		AdColony.Configure
+			(
+				"version:0.1,store:google", // Arbitrary app version and Android app store declaration.
+				"app5f0c212e852d435d9b",   // ADC App ID from adcolony.com
+				"vze08d53ef040643c598" // A zone ID from adcolony.com
+				);
+	}
+
+	public void OnVideoFinished(bool ad_was_shown)
+	{
+		Debug.Log("On Video Finished");
+		// Resume your app here.
+		
+		// MAYBE A THANK YOU MESSAGE
+	}
+}
---
+++ PickItOut/Assets/Scripts/PersistantData.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class PersistantData : MonoBehaviour {
+
+	public static bool debugMode = false;
+	public static UserData userData;
+
+	// Use this for initialization
+	void Awake () {
+		BinaryFormatter bf = new BinaryFormatter ();
+		if (File.Exists (Application.persistentDataPath + "/PickItOut.dat")) {
+			FileStream file = File.Open (Application.persistentDataPath + "/PickItOut.dat", FileMode.Open);
+			PersistantData.userData = (UserData)bf.Deserialize (file);
+			file.Close ();
+		} else {
+			PersistantData.userData = new UserData();
+			FileStream file = File.Open (Application.persistentDataPath + "/PickItOut.dat", FileMode.Create);
+			bf.Serialize(file, PersistantData.userData);
+			file.Close ();
+		}
+	}
+
+	public static void Save() {
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Open (Application.persistentDataPath + "/PickItOut.dat", FileMode.Create);
+		bf.Serialize(file, PersistantData.userData);
+		file.Close ();
+	}
+
+	public static void ClearData() {
+		BinaryFormatter bf = new BinaryFormatter ();
+		PersistantData.userData = new UserData();
+		FileStream file = File.Open (Application.persistentDataPath + "/PickItOut.dat", FileMode.Create);
+		bf.Serialize(file, PersistantData.userData);
+		file.Close ();
+	}
+}
+
+[Serializable]
+public class UserData {
+	public DateTime lastAd = DateTime.MinValue;
+	public int numAdsWatched = 0;
+}
